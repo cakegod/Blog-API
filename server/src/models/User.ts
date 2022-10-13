4f15bd534723b98245314187e95fd5741b72fb99
Cake <cakemancerr@gmail.com>
2022-10-13 16:53:50 +0200

    feat: add user model

---
+++ server/src/models/User.ts
@@ -0,0 +1,15 @@
+import { Schema, model } from 'mongoose';
+
+interface IUser {
+	username: string;
+	password: string;
+}
+
+const UserSchema = new Schema<IUser>({
+	username: { type: String, required: true },
+	password: { type: String, required: true },
+});
+
+const User = model<IUser>('User', UserSchema);
+
+export { User, IUser };
