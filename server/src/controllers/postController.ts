b1fea3100928bceabe5893ce4383990d42132a36
Cake <cakemancerr@gmail.com>
2022-10-13 14:12:29 +0200

    feat: add post controllers

---
+++ server/src/controllers/postController.ts
@@ -0,0 +1,128 @@
+import async from 'async';
+import { Request, Response, NextFunction } from 'express';
+import { body, check, validationResult } from 'express-validator';
+import bcrypt from 'bcryptjs';
+import passport from 'passport';
+
+import { CallbackError } from 'mongoose';
+import { Post } from '../models/Post';
+
+const postController = {
+	getPosts: (req: Request, res: Response, next: NextFunction) =>
+		Post.find({}).exec((err, result) => {
+			if (err) {
+				return next(err);
+			}
+			res.json(result);
+		}),
+
+	postPost: [
+		check('title', 'Title must not be empty')
+			.not()
+			.isEmpty()
+			.trim()
+			.escape(),
+		check('description', 'Description must not be empty')
+			.not()
+			.isEmpty()
+			.trim()
+			.escape(),
+		check('content', 'Content must not be empty')
+			.not()
+			.isEmpty()
+			.trim()
+			.escape(),
+		(req: Request, res: Response, next: NextFunction) => {
+			const errors = validationResult(req);
+			if (!errors.isEmpty()) {
+				console.log(req.body);
+				res.json({
+					title: req.body.title,
+					description: req.body.description,
+					content: req.body.content,
+					errors: errors.array(),
+				});
+				return;
+			}
+			const post = new Post({
+				title: req.body.title,
+				description: req.body.description,
+				content: req.body.content,
+				date: Date.now(),
+			});
+
+			post.save(err => {
+				if (err) {
+					return next(err);
+				}
+			});
+
+			res.status(201).end();
+		},
+	],
+
+	getPost: (req: Request, res: Response, next: NextFunction) =>
+		Post.findById(req.params.postid).exec((err, result) => {
+			if (err) {
+				return next(err);
+			}
+			res.json(result);
+		}),
+
+	putPost: [
+		check('title', 'Title must not be empty')
+			.not()
+			.isEmpty()
+			.trim()
+			.escape(),
+		check('description', 'Description must not be empty')
+			.not()
+			.isEmpty()
+			.trim()
+			.escape(),
+		check('content', 'Content must not be empty')
+			.not()
+			.isEmpty()
+			.trim()
+			.escape(),
+		(req: Request, res: Response, next: NextFunction) => {
+			const errors = validationResult(req);
+			if (!errors.isEmpty()) {
+				res.status(400).json({
+					title: req.body.title,
+					description: req.body.description,
+					content: req.body.content,
+					errors: errors.array(),
+				});
+				return;
+			}
+			const post = new Post({
+				title: req.body.title,
+				description: req.body.description,
+				content: req.body.content,
+				_id: req.body.id,
+				date: req.body.date,
+				published: true,
+			});
+
+			console.log(post, req.body);
+
+			Post.findByIdAndUpdate(req.params.postid, post, {}, err => {
+				if (err) {
+					return next(err);
+				}
+
+				res.status(200).end();
+			});
+		},
+	],
+	deletePost: (req: Request, res: Response) =>
+		Post.findByIdAndDelete(req.params.postid, (err: CallbackError) => {
+			if (err) {
+				return err;
+			}
+			res.status(200).end();
+		}),
+};
+
+export default postController;
