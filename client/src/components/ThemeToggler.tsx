07e339942f3d0e440eed8462532928ebb0720296
Cake <cakemancerr@gmail.com>
2022-10-14 17:44:16 +0200

    feat: add theme toggler

--- client/src/components/ThemeToggler.tsx
+++ client/src/components/ThemeToggler.tsx
@@ -1,18 +1,26 @@
-import React from 'react';
 import { MoonIcon, SunIcon } from '@heroicons/react/24/solid';
+import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
 function ThemeToggler() {
-  const isDarkMode = true;
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+  console.log(theme);
   return (
     <div>
-      {isDarkMode ? (
-        <button>
-          <MoonIcon className='w-8 text-indigo-400' />
-        </button>
-      ) : (
-        <button>
+      <button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
+        {theme === 'light' ? (
           <SunIcon className='w-8 text-amber-400' />
-        </button>
-      )}
+        ) : (
+          <MoonIcon className='w-8 text-indigo-400' />
+        )}
+      </button>
     </div>
   );
 }
--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -2,16 +2,19 @@ import './styles/global.css';
 import Head from 'next/head';
 import { AppProps } from 'next/app';
 import Header from '@/components/Header';
+import { ThemeProvider } from 'next-themes';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <ThemeProvider attribute='class'>
       <Head>
         <title></title>
         <meta></meta>
       </Head>
       <Header />
-      <Component {...pageProps} />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   );
 }
