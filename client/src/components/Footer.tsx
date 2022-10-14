9421e90282355dd819ac979f5a15f71f076128ca
Cake <cakemancerr@gmail.com>
2022-10-14 20:42:59 +0200

    feat: add footer

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className='border-t pt-20 border-gray-300 dark:border-gray-700 dark:text-gray-400 text-gray-700'>
+      All rights reserved © Cake 2022
+    </footer>
+  );
+}
+
+export default Footer;
