8fec63cd9d4d0240ec265eaa9755d053d8b53cd9
Cake <cakemancerr@gmail.com>
2022-10-14 16:36:18 +0200

    feat: add home posts fetch

---
+++ client/src/pages/blog/index.tsx
@@ -0,0 +1,32 @@
+import AboutMe from '@/components/AboutMe';
+import Header from '@/components/Header';
+import Posts from '@/components/Posts';
+import { IPost } from '@/types';
+import { useEffect, useState } from 'react';
+
+export default function HomePage() {
+  const [postsData, setPostData] = useState<IPost[]>();
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch(`http://localhost:3000/blog`);
+        const data = await res.json();
+        console.log(data);
+        setPostData(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchPosts();
+  }, []);
+
+  return (
+    <>
+      <AboutMe></AboutMe>
+      <main className='flex flex-col gap-10'>
+        <Posts postsData={postsData} />
+      </main>
+    </>
+  );
+}
