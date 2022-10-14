89768881144d5b56f1b16e6b56cd04c0331301c8
Cake <cakemancerr@gmail.com>
2022-10-14 16:39:27 +0200

    feat: improve styles

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,15 +1,23 @@
 import Link from 'next/link';
 import React from 'react';
+import ThemeToggler from './ThemeToggler';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
 
 function Header() {
   return (
-    <header className='pb-6'>
-      <h1 className='text-4xl font-semibold pb-3 dark:text-gray-100'>
-        Cake's Blog
-      </h1>
-      <nav className='font-medium flex justify-between dark:text-gray-300'>
-        <Link href='/blog'>Blog</Link>
-        <Link href='/github'>Github</Link>
+    <header className='pb-12'>
+      <div className='flex justify-between items-center'>
+        <h1 className='text-4xl md:text-5xl font-semibold pb-6 dark:text-gray-100'>
+          Cake's Blog
+        </h1>
+        {/* <button className='bg-gray-600 rounded-full p-1'>
+          <MagnifyingGlassIcon className='w-6 ' />
+        </button> */}
+        <ThemeToggler />
+      </div>
+      <nav className='font-medium text-lg md:text-xl flex justify-between dark:text-gray-300 dark:hover:[&>*]:text-purple-400 dark:[&>*]:transition-all'>
+        <Link href='/'>Blog</Link>
+        <Link href='https://github.com/cakegod'>Github</Link>
         <Link href='/portfolio'>Portfolio</Link>
         <Link href='/contact'>Contact</Link>
       </nav>
--- client/src/components/Posts.tsx
+++ client/src/components/Posts.tsx
@@ -1,4 +1,5 @@
 import { IPost } from '@/types';
+import Link from 'next/link';
 import React from 'react';
 
 interface Props {
@@ -7,27 +8,36 @@ interface Props {
 
 function Posts({ postsData }: Props) {
   return postsData === undefined ? (
-    <h2 className='text-4xl'>There are currently no posts available</h2>
+    <h2 className='text-2xl dark:text-gray-100'>
+      There are currently no posts available
+    </h2>
   ) : (
-    postsData.map((post) => {
-      return (
-        <section key={post._id} className='pb-6'>
-          <h2 className='text-xl text-amber-700 dark:text-amber-400 font-semibold'>
-            {post.title}
-          </h2>
-          <h3 className='text-sm text-gray-900 dark:text-gray-100'>
-            {new Intl.DateTimeFormat('en-GB', {
-              year: 'numeric',
-              month: 'long',
-              day: '2-digit',
-            }).format(Date.parse(post.date))}
-          </h3>
-          <h3 className='text-lg text-gray-900 dark:text-gray-100'>
-            {post.description}
-          </h3>
-        </section>
-      );
-    })
+    <>
+      {postsData.map((post) => {
+        return (
+          <Link
+          key={post._id}
+            href={`/blog/${post._id}`}
+          >
+            <section  className='cursor-pointer'>
+              <h2 className='text-xl md:text-2xl text-amber-700 dark:text-amber-400 font-semibold hover:hue-rotate-180 contrast-[.85] transition-all duration-500'>
+                {post.title}
+              </h2>
+              <h3 className='text-sm text-gray-900 dark:text-gray-300'>
+                {new Intl.DateTimeFormat('en-GB', {
+                  year: 'numeric',
+                  month: 'long',
+                  day: '2-digit',
+                }).format(Date.parse(post.date))}
+              </h3>
+              <h3 className='text-lg text-gray-900 dark:text-gray-100'>
+                {post.description}
+              </h3>
+            </section>
+          </Link>
+        );
+      })}
+    </>
   );
 }
 
