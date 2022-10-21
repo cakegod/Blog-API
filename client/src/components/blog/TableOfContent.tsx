5d60a9c6deff8423217555cadb5815a587ed708b
Cake <cakemancerr@gmail.com>
2022-10-21 20:07:34 +0200

    fix: table of content return

--- client/src/components/blog/TableOfContent.tsx
+++ client/src/components/blog/TableOfContent.tsx
@@ -9,24 +9,28 @@ function TableOfContent() {
         heading.setAttribute('id', `${heading.textContent}`)
       );
       const createArray = Array.from(query);
-      const getHeadings = createArray.map((heading) => heading.textContent) as string[];
+      const getHeadings = createArray.map(
+        (heading) => heading.textContent
+      ) as string[];
       setHeadings(getHeadings);
     }
   }, []);
 
   return (
-    headings && (
-      <div className='px-4 py-1 left-0 top-0'>
-        <h4 className='uppercase'>Table of Content</h4>
-        <ul>
-          {headings.map((heading) => (
-            <li className='text-base'>
-              <a href={`#${heading}`}>{heading}</a>
-            </li>
-          ))}
-        </ul>
-      </div>
-    )
+    <>
+      {headings && (
+        <div className='px-4 py-1 left-0 top-0'>
+          <h4 className='uppercase'>Table of Content</h4>
+          <ul>
+            {headings.map((heading) => (
+              <li className='text-base'>
+                <a href={`#${heading}`}>{heading}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
   );
 }
 
