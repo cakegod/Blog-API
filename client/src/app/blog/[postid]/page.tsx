c17bf36c888cd362407574c6959477b41943b0ca
Cake <cakemancerr@gmail.com>
2022-10-28 23:14:05 +0200

    chore: improve post composition

--- client/src/app/blog/[postid]/page.tsx
+++ client/src/app/blog/[postid]/page.tsx
@@ -3,6 +3,7 @@ import TableOfContent from '@/components/blog/TableOfContents';
 import formatDate from '@/components/util/formatData';
 import ArticleContent from '@/components/blog/ArticleContent';
 import CommentSytem from '@/components/blog/CommentSytem';
+import ArticleHeader from '@/components/blog/ArticleHeader';
 
 type Params = { params: { postid: string } };
 
@@ -25,25 +26,12 @@ async function Post({ params }: Params) {
   const post = await fetchPost(params);
 
   return (
-    <div className='py-12'>
-      <article className='dark: prose prose-violet max-w-[100%] hover:prose-a:text-pink-500 hover:prose-a:transition-colors prose-pre:p-3 prose-pre:text-lg prose-pre:contrast-[1.05] dark:prose-invert dark:contrast-[.85] dark:prose-a:text-violet-400 dark:hover:prose-a:text-pink-400 dark:prose-pre:contrast-125 md:prose-lg md:prose-pre:text-xl'>
-        {
-          <>
-            <div>
-              <h2 className='m-0 text-violet-700 dark:text-violet-400'>
-                {post.title}
-              </h2>
-              <p className='font-medium'>
-                {formatDate(post.date, post.readTime)}
-              </p>
-            </div>
-            <TableOfContent />
-            <ArticleContent post={post} />
-          </>
-        }
-      </article>
+    <>
+      <ArticleHeader post={post} />
+      <TableOfContent />
+      <ArticleContent post={post} />
       <CommentSytem post={post} />
-    </div>
+    </>
   );
 }
 
