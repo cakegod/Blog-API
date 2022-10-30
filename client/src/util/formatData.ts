e19e6b7618f9cc7c8577dfb6863767738223144c
Cake <cakemancerr@gmail.com>
2022-10-31 00:55:40 +0100

    chore: improve util func readability

--- client/src/util/formatData.ts
+++ client/src/util/formatData.ts
@@ -1,17 +1,21 @@
-const timePerCoffee = 6;
+const TIME_PER_COFFEE = 6;
 
-function computeTimeInCoffees(readTime: number) {
-  return '☕'.repeat(Math.round(readTime / timePerCoffee));
+function computeTimeInCoffees(readTime: number): string{
+  const coffeeIcon = '☕'
+  const numberOfCoffees = Math.round(readTime / TIME_PER_COFFEE)
+  return coffeeIcon.repeat(numberOfCoffees);
 }
 
-function formatDate(date: string, readTime: string): string {
-  return `${new Intl.DateTimeFormat('en-GB', {
-    year: 'numeric',
-    month: 'long',
-    day: '2-digit',
-  }).format(Date.parse(date))} - ${readTime} ${computeTimeInCoffees(
-    parseInt(readTime, 10)
-  )}`;
+function formatData(date: string, readTime: string) {
+const parsedDate = Date.parse(date);
+const parsedReadTime = parseInt(readTime, 10)
+const customDateFormat = new Intl.DateTimeFormat('en-GB', {
+  year: 'numeric',
+  month: 'long',
+  day: '2-digit',
+})
+const formattedDate = customDateFormat.format(parsedDate)
+return `${formattedDate} - ${readTime} ${computeTimeInCoffees(parsedReadTime)}`
 }
 
-export default formatDate;
+export default formatData;
