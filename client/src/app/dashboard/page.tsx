e394d860050d005ce01ffd533a2f9552f6358ae4
Cake <cakemancerr@gmail.com>
2022-12-07 18:59:37 +0100

    feat: add fetch

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+"use client"
+import { PostProps } from "@/types";
+import React, { useEffect } from "react";
 
-function Dashboard() {
-  return <div className='py-12'></div>;
+
+async function fetchPosts(): Promise<PostProps> {
+  const url = `${process.env.URL}/dashboard/`;
+  const res = await fetch(url);
+  const data = await res.json();
+
+  return data;
+}
+
+async function Dashboard() {
+  const posts = await fetchPosts()
+
+  useEffect(() => {
+    console.log(posts);;
+  
+ 
+  }, [posts]);
+  
+
+  return (<div className='py-12'></div>);
 }
 
 export default Dashboard;
