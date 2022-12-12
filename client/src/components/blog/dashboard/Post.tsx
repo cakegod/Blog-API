e4d2d4a875d27ddcaf9085fb7423d3ea6445b222
Cake <cakemancerr@gmail.com>
2022-12-12 21:43:39 +0100

    feat: add value to buttons

--- client/src/components/blog/dashboard/Post.tsx
+++ client/src/components/blog/dashboard/Post.tsx
@@ -2,20 +2,22 @@ import { PostProps } from '@/types';
 import React from 'react';
 
 interface Props {
-  post: PostProps
+  post: PostProps;
 }
 
 function Post({ post }: Props) {
   return (
     <section key={post._id} className='m-4 grid grid-cols-3'>
       <h3 className=''>{post.title}</h3>
-      <p className=''>
         {post.published ? (
-          <button className='rounded bg-blue-900 p-2'>published</button>
+          <form action='' method='post'>
+            <button className='rounded bg-blue-900 p-2' value={post._id} type='submit'>published</button>
+          </form>
         ) : (
-          <button className='rounded bg-red-900 p-2'>not published</button>
+          <form action='' method='post'>
+          <button className='rounded bg-red-900 p-2' value={post._id} type='submit'>unpublished</button>
+        </form>
         )}
-      </p>
       <h4 className=''>{post.date}</h4>
     </section>
   );
