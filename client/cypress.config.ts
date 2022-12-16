2bb2043e1557c384e2ea7217f34a7ee6eac74518
Cake <cakemancerr@gmail.com>
2022-12-16 19:46:06 +0100

    feat: add cypress

---
+++ client/cypress.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  projectId: '3xpybr',
+  e2e: {
+    specPattern: 'cypress/e2e/*.cy.{js,jsx,ts,tsx}',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
