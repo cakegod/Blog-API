83a6ce6c08ddde3f7dbc9051d11c901f8b8e5f2c
Cake <cakemancerr@gmail.com>
2022-12-17 22:15:10 +0100

    chore: update cypress config

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from 'cypress';
+import { defineConfig } from "cypress";
 
 export default defineConfig({
-  projectId: '3xpybr',
+  projectId: "3xpybr",
+
   e2e: {
-    specPattern: 'cypress/e2e/*.cy.{js,jsx,ts,tsx}',
+    specPattern: "cypress/e2e/*.cy.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
     },
   },
 });
